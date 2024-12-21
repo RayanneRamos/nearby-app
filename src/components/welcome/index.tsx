1a67db5a5111b2e5b04e4e1bd050c3d74106f7c4
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-21 16:19:59 -0300

    :sparkles: feat: create a welcome component

---
+++ src/components/welcome/index.tsx
@@ -0,0 +1,14 @@
+import { Image, Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Welcome() {
+  return (
+    <View>
+      <Image style={styles.logo} source={require("@/assets/logo.png")} />
+      <Text style={styles.title}>Boas vindas ao Nearby!</Text>
+      <Text style={styles.subtitle}>
+        Tenha cupons de vantagem para usar em seus estabelecimentos favoritos.
+      </Text>
+    </View>
+  );
+}
