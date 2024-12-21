f8a48863943c7d6e0e8dc7a149f2ccaeb55defd5
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-21 17:06:08 -0300

    :label: creating the step component typing

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -1,37 +1,21 @@
 import { colors } from "@/styles/colors";
-import { IconMapPin, IconQrcode, IconTicket } from "@tabler/icons-react-native";
+import { IconProps } from "@tabler/icons-react-native";
 import { Text, View } from "react-native";
 import { styles } from "./styles";
 
-export function Step() {
+interface StepProps {
+  icon: React.ComponentType<IconProps>;
+  title: string;
+  description: string;
+}
+
+export function Step({ title, description, icon: Icon }: StepProps) {
   return (
-    <View>
-      <View style={styles.container}>
-        <IconMapPin size={32} color={colors.red.base} />
-        <View style={styles.details}>
-          <Text style={styles.title}>Encontre estabelecimentos</Text>
-          <Text style={styles.description}>
-            Veja os locais perto de você que são parceiros Nearby
-          </Text>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <IconQrcode size={32} color={colors.red.base} />
-        <View style={styles.details}>
-          <Text style={styles.title}>Ative o cupom com QR Code</Text>
-          <Text style={styles.description}>
-            Escaneie o código no estabelecimento para usar o benefício
-          </Text>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <IconTicket size={32} color={colors.red.base} />
-        <View style={styles.details}>
-          <Text style={styles.title}>Garanta as vantagens perto de você</Text>
-          <Text style={styles.description}>
-            Ative cupons onde estiver, em diferentes tipos de estabelecimento
-          </Text>
-        </View>
+    <View style={styles.container}>
+      {Icon && <Icon size={32} color={colors.red.base} />}
+      <View style={styles.details}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
       </View>
     </View>
   );
