19276956c6bd5eeec212dde839dee00293f8a5d5
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-21 17:22:43 -0300

    :recycle: refactor: modifying the button structure

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,15 +1,31 @@
-import { Text, TouchableOpacity } from "react-native";
+import {
+  ActivityIndicator,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
 import { styles } from "./styles";
 import { Children, ReactNode } from "react";
+import { colors } from "@/styles/colors";
+import { TextProps } from "react-native-svg";
 
-interface ButtonProps {
-  children: ReactNode;
+interface ButtonProps extends TouchableOpacityProps {
+  isLoading?: false;
 }
 
-export function Button({ children }: ButtonProps) {
+function Button({ children, style, isLoading = false, ...props }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.container}>
-      <Text style={styles.title}>{children}</Text>
+    <TouchableOpacity
+      style={[(styles.container, style)]}
+      activeOpacity={0.8}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? (
+        <ActivityIndicator size="small" color={colors.gray[100]} />
+      ) : (
+        children
+      )}
     </TouchableOpacity>
   );
 }
