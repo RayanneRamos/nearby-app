7f322d04419367a256e271c98c229fe18a45f611
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-22 20:20:23 -0300

    :label: creating the category component typing

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,9 +1,31 @@
-import { Pressable, Text } from "react-native";
+import { Pressable, PressableProps, Text } from "react-native";
+import { styles } from "./styles";
+import { categoriesIcons } from "@/utils/categories-icons";
+import { colors } from "@/styles/colors";
 
-export function Category() {
+interface CategoryProps extends PressableProps {
+  iconId: string;
+  isSelected?: boolean;
+  name: string;
+}
+
+export function Category({
+  iconId,
+  isSelected = false,
+  name,
+  ...props
+}: CategoryProps) {
+  const Icon = categoriesIcons[iconId];
+
   return (
-    <Pressable>
-      <Text>Alimentação</Text>
+    <Pressable
+      style={[styles.container, isSelected && styles.containerSelected]}
+      {...props}
+    >
+      <Icon size={16} color={colors.gray[isSelected ? 100 : 400]} />
+      <Text style={[styles.name, isSelected && styles.nameSelected]}>
+        {name}
+      </Text>
     </Pressable>
   );
 }
