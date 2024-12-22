554b031bdecbb3e64ce096678bb9d76fc30492ed
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-22 20:21:37 -0300

    :label: creating the categories component typing

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,5 +1,17 @@
 import { FlatList, Text } from "react-native";
 
+export interface CategoriesProps {
+  id: string;
+  name: string;
+}
+[];
+
+interface Props {
+  data: CategoriesProps;
+  selected?: string;
+  onSelect: (id: string) => void;
+}
+
 export function Categories() {
   return <FlatList />;
 }
