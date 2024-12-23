fdd852871021ed7ce44bfc94012999bf63bc2a01
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-22 21:05:44 -0300

    :recycle: refactor: modifying the categories structure

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,17 +1,35 @@
 import { FlatList, Text } from "react-native";
+import { Category } from "../category";
+import { styles } from "./styles";
 
 export interface CategoriesProps {
   id: string;
   name: string;
 }
-[];
 
 interface Props {
-  data: CategoriesProps;
+  data: CategoriesProps[];
   selected?: string;
   onSelect: (id: string) => void;
 }
 
-export function Categories() {
-  return <FlatList />;
+export function Categories({ data, selected, onSelect }: Props) {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <Category
+          name={item.name}
+          iconId={item.id}
+          onPress={() => onSelect(item.id)}
+          isSelected={item.id === selected}
+        />
+      )}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={styles.content}
+      style={styles.container}
+    />
+  );
 }
