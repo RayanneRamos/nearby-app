94d0293ca47bc67f9ba717904e30786363eab130
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-23 16:17:04 -0300

    :sparkles: feat: getting information from page market id dynamically

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/button";
+import { Loading } from "@/components/loading";
+import { Coupon } from "@/components/market/coupon";
 import { Cover } from "@/components/market/cover";
 import { Details, DetailsProps } from "@/components/market/details";
 import { api } from "@/services/api";
 import { CameraView, useCameraPermissions } from "expo-camera";
-import { router, useLocalSearchParams } from "expo-router";
+import { Redirect, router, useLocalSearchParams } from "expo-router";
 import { useEffect, useRef, useState } from "react";
 import {
   Alert,
@@ -89,26 +91,47 @@ export default function Market() {
     );
   }
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!data) {
+    return <Redirect href="/home" />;
+  }
+
   useEffect(() => {
     fetchMarket();
-  }, [params.id]);
+  }, [params.id, coupon]);
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="light-content" hidden={isVisibleCameraModal} />
       <ScrollView showsVerticalScrollIndicator={false}>
-        <Cover uri={""} />
-        <Details data={[]} />
+        <Cover uri={data.cover} />
+        <Details data={data} />
+        {coupon && <Coupon code="FM345T6" />}
       </ScrollView>
       <View style={styles.buttonContainer}>
-        <Button>
+        <Button onPress={handleOpenCamera}>
           <Button.Title>Ler QR Code</Button.Title>
         </Button>
       </View>
-      <Modal style={styles.modal}>
-        <CameraView />
-        <View>
-          <Button>
+      <Modal style={styles.modal} visible={isVisibleCameraModal}>
+        <CameraView
+          style={{ flex: 1 }}
+          facing="back"
+          onBarcodeScanned={({ data }) => {
+            if (data && !qrLock.current) {
+              (qrLock.current = true),
+                setTimeout(() => handleUseCoupon(data), 500);
+            }
+          }}
+        />
+        <View style={{ position: "absolute", bottom: 32, left: 32, right: 32 }}>
+          <Button
+            onPress={() => setIsVisibleCameraModal(false)}
+            isLoading={couponIsFetching}
+          >
             <Button.Title>Voltar</Button.Title>
           </Button>
         </View>
