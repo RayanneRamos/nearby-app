ee393a6167914e0c25032cc49eabac9d88d3a5d1
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-22 21:33:02 -0300

    :sparkles: feat: getting information from component place dynamically

--- src/components/place/index.tsx
+++ src/components/place/index.tsx
@@ -22,18 +22,16 @@ interface Props extends TouchableOpacityProps {
   data: PlaceProps;
 }
 
-export function Place() {
+export function Place({ data, ...props }: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
-      <Image style={styles.image} />
+    <TouchableOpacity style={styles.container} {...props}>
+      <Image style={styles.image} source={{ uri: data.cover }} />
       <View style={styles.content}>
-        <Text style={styles.name}>RocketBurger</Text>
-        <Text style={styles.description}>
-          Na compra de um combo SuperRocket, leve outro combo.
-        </Text>
+        <Text style={styles.name}>{data.name}</Text>
+        <Text style={styles.description}>{data.description}</Text>
         <View style={styles.footer}>
           <IconTicket size={16} color={colors.red.base} />
-          <Text style={styles.tickets}>3 cupons disponíveis</Text>
+          <Text style={styles.tickets}>{data.coupons} cupons disponíveis</Text>
         </View>
       </View>
     </TouchableOpacity>
