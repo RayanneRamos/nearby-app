56f63e50d2819ea8d9c59983873b0e85f970ddc1
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-23 11:19:32 -0300

    :lipstick: style: create a cover styles

--- src/components/market/cover/styles.ts
+++ src/components/market/cover/styles.ts
@@ -1,7 +1,16 @@
+import { colors } from "@/styles/colors";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    width: "100%",
+    height: 232,
+    marginBottom: -32,
+    backgroundColor: colors.gray[200],
+  },
 
-  header: {},
+  header: {
+    padding: 24,
+    paddingTop: 56,
+  },
 });
