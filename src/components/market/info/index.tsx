ddebe2bc01fc1417b8daa67fae63865a9083ee2e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-23 15:34:27 -0300

    :label: creating the info typing

--- src/components/market/info/index.tsx
+++ src/components/market/info/index.tsx
@@ -1,10 +1,17 @@
 import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
+import { IconProps } from "@tabler/icons-react-native";
+import { colors } from "@/styles/colors";
 
-export function Info() {
+interface InfoProps {
+  description: string;
+  icon: React.ComponentType<IconProps>;
+}
+
+export function Info({ description, icon: Icon }: InfoProps) {
   return (
     <View style={styles.container}>
-      <Image />
+      <Icon size={16} color={colors.gray[400]} />
       <Text style={styles.text}>oi</Text>
     </View>
   );
