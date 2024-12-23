e019853419a1d2e720cbcc33224311a783ada0fb
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-23 15:32:38 -0300

    :sparkles: feat: create a info component

---
+++ src/components/market/info/index.tsx
@@ -0,0 +1,11 @@
+import { Image, Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Info() {
+  return (
+    <View style={styles.container}>
+      <Image />
+      <Text style={styles.text}>oi</Text>
+    </View>
+  );
+}
