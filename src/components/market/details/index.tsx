36af367dfccf54fa51efa0c42eb369a25cbd8876
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-23 15:40:24 -0300

    :sparkles: feat: create a details component

---
+++ src/components/market/details/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View } from "react-native";
+import { Info } from "../info";
+import { IconMapPin, IconPhone, IconTicket } from "@tabler/icons-react-native";
+import { styles } from "./styles";
+
+export function Details() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>Nome</Text>
+      <Text style={styles.description}>Descrição</Text>
+      <View style={styles.group}>
+        <Text style={styles.title}>Informações</Text>
+        <Info icon={IconTicket} description="3 cupons disponíveis" />
+        <Info icon={IconMapPin} description="Rua abc 10" />
+        <Info icon={IconPhone} description="999999999" />
+      </View>
+      <View style={styles.group}>
+        <Text style={styles.title}>Regulamento</Text>
+        <Text style={styles.rule}>Descrição</Text>
+      </View>
+    </View>
+  );
+}
