e720290e68950a451932d5f25e53d2039d0f35a6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-12-23 15:55:31 -0300

    :sparkles: feat: create a market id page

---
+++ src/app/market/[id].tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/button";
+import { Cover } from "@/components/market/cover";
+import { Details } from "@/components/market/details";
+import { CameraView } from "expo-camera";
+import { Modal, ScrollView, StatusBar, StyleSheet, View } from "react-native";
+
+export default function Market() {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Cover uri={""} />
+        <Details data={[]} />
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <Button>
+          <Button.Title>Ler QR Code</Button.Title>
+        </Button>
+      </View>
+      <Modal style={styles.modal}>
+        <CameraView />
+        <View>
+          <Button>
+            <Button.Title>Voltar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  buttonContainer: {
+    padding: 32,
+  },
+
+  modal: {
+    flex: 1,
+  },
+});
