b6bd4890646da5db3228e17996c772513f5ff3af
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-03 15:03:42 -0300

    :sparkles: feat: create a coupon component

---
+++ src/components/coupon/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+import { IconTicket } from "@tabler/icons-react-native";
+import { colors } from "@/styles/colors";
+import { DetailsProps } from "../market/details";
+
+interface CouponProps {
+  data: DetailsProps;
+}
+
+export function Coupon({ data }: CouponProps) {
+  return (
+    <View style={styles.couponContainer}>
+      <IconTicket size={24} color={colors.red.base} />
+      <View style={styles.couponNameContainer}>
+        <Text style={styles.couponQuantity}>{data.coupons}</Text>
+        <Text style={styles.couponName}>cupons disponíveis</Text>
+      </View>
+    </View>
+  );
+}
