4e9cf3c813c48e2f6712b0cf80ffc4740fbfa256
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-03 15:04:54 -0300

    :sparkles: feat: using a coupon component in file details

--- src/components/market/details/index.tsx
+++ src/components/market/details/index.tsx
@@ -1,7 +1,14 @@
 import { Text, View } from "react-native";
 import { Info } from "../info";
-import { IconMapPin, IconPhone, IconTicket } from "@tabler/icons-react-native";
+import {
+  IconMapPin,
+  IconPhone,
+  IconTicket,
+  IconToolsKitchen2,
+} from "@tabler/icons-react-native";
 import { styles } from "./styles";
+import { colors } from "@/styles/colors";
+import { Coupon } from "@/components/coupon";
 
 export interface DetailsProps {
   name: string;
@@ -22,16 +29,16 @@ interface Props {
 export function Details({ data }: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{data.name}</Text>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{data.name}</Text>
+        <IconToolsKitchen2 size={24} color={colors.green.light} />
+      </View>
       <Text style={styles.description}>{data.description}</Text>
+      <Coupon data={data} />
       <View style={styles.group}>
         <Text style={styles.title}>Informações</Text>
-        <Info
-          icon={IconTicket}
-          description={`${data.coupons} cupons disponíveis`}
-        />
         <Info icon={IconMapPin} description={data.address} />
-        <Info icon={IconPhone} description={data.phone} />
+        <Info icon={IconPhone} description={`+55 ${data.phone}`} />
       </View>
       <View style={styles.group}>
         <Text style={styles.title}>Regulamento</Text>
