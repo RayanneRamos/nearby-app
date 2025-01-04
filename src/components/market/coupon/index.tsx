87d9f37b699b52fcf48d7fcf28cc33603a522f01
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-04 16:35:16 -0300

    :sparkles: feat: create the clipboardToCoupon function

--- src/components/market/coupon/index.tsx
+++ src/components/market/coupon/index.tsx
@@ -1,5 +1,5 @@
 import { IconTicket } from "@tabler/icons-react-native";
-import { Text, View } from "react-native";
+import { Alert, Clipboard, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
 
@@ -8,12 +8,23 @@ interface CouponProps {
 }
 
 export function Coupon({ code }: CouponProps) {
+  async function handleCopyCouponToClipboard() {
+    await Clipboard.setString(code);
+    Alert.alert("Sucesso", "Cupom copiado para a àrea de transferência.");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Utilize esse cupom</Text>
       <View style={styles.content}>
-        <IconTicket size={24} color={colors.green.light} />
-        <Text style={styles.code}>{code}</Text>
+        <TouchableOpacity
+          onPress={handleCopyCouponToClipboard}
+          activeOpacity={0.8}
+          style={styles.buttonClipboard}
+        >
+          <IconTicket size={24} color={colors.green.light} />
+          <Text style={styles.code}>{code}</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
